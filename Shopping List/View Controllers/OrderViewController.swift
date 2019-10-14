8f37d8b3af00bbc4cb10d4a1f9e57d07df9d08b3
Craig Swanson <cswanson02@me.com>
2019-10-14 00:21:27 -0500

    Added Order View Controller and segue.  Set up scene.

---
+++ Shopping List/View Controllers/OrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderViewController.swift
+//  Shopping List
+//
+//  Created by Craig Swanson on 10/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
